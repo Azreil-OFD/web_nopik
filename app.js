d740f7a5b3a1c831e2696f1e980329eb0a1a6f27
Azreil-OFD <fantom2413@gmail.com>
2022-10-20 00:46:07 +0300

    add table

--- app.js
+++ app.js
@@ -1,34 +1,80 @@
 let tg = window.Telegram.WebApp; //получаем объект webapp телеграма
-document.getElementById('date').valueAsDate = new Date();
+document.getElementById("date").valueAsDate = new Date();
 tg.expand(); //расширяем на все окно
 
 tg.MainButton.text = "Показать расписание"; //изменяем текст кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
-tg.MainButton.show()
+tg.MainButton.setParams({ color: "#143F6B" }); //так изменяются все параметры
+tg.MainButton.show();
 
 var day = true;
+var dayPicker = () => {
+    const myHeaders = new Headers();
+  myHeaders.append("Accept", "application/json");
 
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    var group = document.getElementById('group').value
-    var date = document.getElementById('date').value
-    if (group != "") {
-        document.getElementById('forms').innerHTML = "тут будет расписание<br/>Для группы:" + group + "<br/>На " + date;
-        if (day) {
-            document.getElementById('forms').innerHTML += 'на день'
-        } else {
-            document.getElementById('forms').innerHTML += 'на неделю'
-        }
+  const myInit = {
+    method: "GET",
+    headers: myHeaders,
+    mode: "cors",
+    cache: "default",
+  };
+
+  var dates  = new Date(date.value);
+  fetch("http://92.255.107.234:5000/schedule/"+ group.value +"/"+dates.toLocaleDateString('ru')+"/day", myInit).then(
+    (response) => {
+
+      response.json().then((e) => {
+        var tables = ""
+        var count = 0
+        e['data'].forEach(element => {
+            tables += "\
+            <tr>\
+                <th scope=\"row\">"+count+"</th>\
+                <td>"+ element['m:UF_ID_SUBJECT'] +"</td>\
+                <td>"+ element['m:UF_ID_TEACHER'] +"</td>\
+                <td>"+ element['m:UF_LECTURE'] +"</td>\
+            </tr>\
+            "
+            count ++
+        });;
+        
+        document.getElementById("forms").innerHTML = "\
+        <table class=\"table table-striped table-dark\">\
+                <thead>\
+                    <tr>\
+                    <th scope=\"col\">Пара</th>\
+                    <th scope=\"col\">Наименование</th>\
+                    <th scope=\"col\">Преподаватель</th>\
+                    <th scope=\"col\">кабинет</th>\
+                    </tr>\
+                </thead>\
+                <tbody>\
+        " + tables + "</tbody> </table>"
+    
+      })
     }
+  );
+}
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+  var group = document.getElementById("group").value;
+  var date = document.getElementById("date").value;
+  if (group != "") {
+    if (day) {
 
+      dayPicker()
+
+    } else {
+      document.getElementById("forms").innerHTML += "на неделю";
+    }
+  }
 });
 
-var dayAndWeek = document.getElementById('dayAndWeek');
+var dayAndWeek = document.getElementById("dayAndWeek");
 
-dayAndWeek.onclick = function() {
-    
-    day = document.getElementById('dayAndWeek').checked
 
-    
-}
- 
+
+
+dayAndWeek.onclick = function () {
+  day = document.getElementById("dayAndWeek").checked;
+  dayPicker()
+};
